86710762e2815b9f2aa5552a81f7b074ab4849af
Asha Patel <asha@promact.com>
2019-06-24 18:50:47 +0530

    home, about section design changes

--- amaze-theme/src/app/app.component.ts
+++ amaze-theme/src/app/app.component.ts
@@ -1,49 +1,74 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, HostListener} from '@angular/core';
 
 /** array properties for menu item
  * @param id for identification of item using number
- * @param name for display name of the menu item
- * @param value is the value of menu item*/
+ * @param name for display name of the menu item*/
 interface menuItem
 {
   id: number;
   name: string;
-  value: string;
 }
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 
 
 export class AppComponent implements OnInit {
 
-  /**instance of the menuItem interface */
+  //instance of the menuItem interface 
   menuItems: menuItem[];
 
-  /*get selected item from menu*/
+  //get selected item from menu
   selectedItem: string="home";
+
+  scrolled: boolean = false;
+  //scroll listner
+  /*show scroll button if y offset is >= 100*/
+  @HostListener('window:scroll')
+  checkScroll() {
+
+   
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+
+    if (scrollPosition > 10) {
+      this.scrolled = true;
+    } else {
+      this.scrolled = false;
+    }
+  }
+
+
 
   /** called after Angular has initialized all data-bound properties of a directive. */
   ngOnInit()
   {
+    //menu item id,name,value
     this.menuItems =
       [
-      { id: 1, name: "HOME", value: "home" },
-      { id: 2, name: "ABOUT", value:"about" },
-      { id: 3, name: "SERVICES", value: "services" },
-      { id: 4, name: "PORTFOLIO", value: "portfolio" },
-      { id: 5, name: "TEAM", value: "team"},
-      { id: 6, name: "PRICING", value: "pricing" },
-      { id: 7, name: "CONTACT", value: "contact" }
+      { id: 1, name:"home" },
+      { id: 2, name:"about" },
+      { id: 3, name:"services" },
+      { id: 4, name:"portfolio" },
+      { id: 5, name:"team"},
+      { id: 6, name: "pricing" },
+      { id: 7, name: "contact" }
       ];
 
-   
+    this.onWindowScroll();
   }
-  
+
+  //get scroll offset
+  onWindowScroll() {
+    const scrollOffset = window.pageYOffset;
+    console.log(scrollOffset);
+  }
 
   /**
    * function call when click on menu item
@@ -51,7 +76,7 @@ export class AppComponent implements OnInit {
    * @param item is selected menu item
    */
   listClick(event, item) {
-    this.selectedItem = item.value;
+    this.selectedItem = item.name;
   }
 
  
